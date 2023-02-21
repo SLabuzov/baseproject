a3a049e4637464185a24bca77950c02a925e98ae
Sergey Labuzov <s.labuzov.public@gmail.com>
2023-02-21 22:04:52 +0300

    Add checkstyle.

--- src/main/java/dev/slabuzov/baseproject/BaseprojectApplication.java
+++ src/main/java/dev/slabuzov/baseproject/BaseprojectApplication.java
@@ -3,11 +3,14 @@ package dev.slabuzov.baseproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Application entry point.
+ */
 @SpringBootApplication
 public class BaseprojectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BaseprojectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BaseprojectApplication.class, args);
+    }
 
 }
